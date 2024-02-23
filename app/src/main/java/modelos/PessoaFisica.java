4a7ee9951961a414e7fef0bbec088684adf26e27
Felipe Romano <54383077+feliperomanovieira@users.noreply.github.com>
2024-02-23 01:39:44 +0000

    Classes Pessoa e PessoaFisica

---
+++ app/src/main/java/modelos/PessoaFisica.java
@@ -0,0 +1,13 @@
+package modelos;
+
+public class PessoaFisica {
+    private int idade;
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+}
